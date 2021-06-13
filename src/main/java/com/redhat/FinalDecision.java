db04e2021f60142510a6de93be8f7302bcaaaa1f
rhdmAdmin <>
2021-06-13 21:42:14 +0100

    {/src/main/java/com/redhat/FinalDecision.java}

--- src/main/java/com/redhat/FinalDecision.java
+++ src/main/java/com/redhat/FinalDecision.java
@@ -7,29 +7,26 @@ package com.redhat;
 @javax.persistence.Entity
 public class FinalDecision implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    @javax.persistence.GeneratedValue(generator = "FINALDECISION_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
-    @javax.persistence.Id
-    @javax.persistence.SequenceGenerator(sequenceName = "FINALDECISION_ID_SEQ", name = "FINALDECISION_ID_GENERATOR")
-    private java.lang.Long id;
+	@javax.persistence.GeneratedValue(generator = "FINALDECISION_ID_GENERATOR", strategy = javax.persistence.GenerationType.AUTO)
+	@javax.persistence.Id
+	@javax.persistence.SequenceGenerator(sequenceName = "FINALDECISION_ID_SEQ", name = "FINALDECISION_ID_GENERATOR")
+	private java.lang.Long id;
 
-    public FinalDecision() {
-    }
-    
-    public FinalDecision(java.lang.Long id) {
-        this.id = id;
-    }
+	public FinalDecision() {
+	}
 
-    public java.lang.Long getId() {
-        return this.id;
-    }
-    
-    public void setId(java.lang.Long id) {
-        this.id = id;
-    }
+	public java.lang.Long getId() {
+		return this.id;
+	}
 
+	public void setId(java.lang.Long id) {
+		this.id = id;
+	}
 
-
+	public FinalDecision(java.lang.Long id) {
+		this.id = id;
+	}
 
 }
